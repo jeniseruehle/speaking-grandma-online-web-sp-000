0fe3d7281d18d865e7b62d31181a68005c26822c
Jenise Ruehle <jela07@gmail.com>
2020-01-28 19:03:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == 
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == 
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return 
+  
     
 end  
 
