5a4d9309052bd258fc1d910e5fd42c0841c812ee
Jenise Ruehle <jela07@gmail.com>
2020-01-28 19:09:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
-    return "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
   end   
 end  
 
